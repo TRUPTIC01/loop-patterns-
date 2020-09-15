112c10f3a421ac27a50ce04bb597c07559344549
TRUPTIC01 <truptichinchole1@gmail.com>
2020-09-16 01:35:32 +0530

    solid square pattern

---
+++ square.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{ 
+	int i,j,n;
+
+	printf("Enter n");
+	scanf("%d",&n);
+
+	for(i = 1; i <= n; i++) 
+	{ 
+		for(j = 1; j<= n; j++)
+		{ 
+			printf("* ");
+		}
+		 
+	     printf("\n");
+	} 
+      
+    return 0;
+}
+
