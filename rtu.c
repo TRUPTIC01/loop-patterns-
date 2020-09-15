55fa9ec98175112c1d2f5fa742475fe3d64e7589
TRUPTIC01 <truptichinchole1@gmail.com>
2020-09-16 02:35:13 +0530

    inverted right triangle

---
+++ rtu.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{ 
+	int i,j,n;
+	
+	printf(" Whats n?");
+	scanf("%d",&n);
+
+	for( i = 1; i <=n; i++)
+	{ 
+		for(j = n; j >= i; j--)
+		{ 
+			printf("* ");
+		} 
+
+		 printf("\n");
+	} 
+
+     return 0;
+}
